50a666f329801390d71ddd5cd72ff37f79a46595
santiago galindo <santiago1593572684@gmail.com>
2024-09-12 01:17:15 +0000

    programa basico para pedir nombre y edad

---
+++ semanauno/lab-1.py
@@ -0,0 +1,8 @@
+def get_average_larguest():
+   name = input("ingrese su nombre: ")
+   age = int(input("ingrese su edad: "))
+
+  
+   print(f"me llamo {name} y tengo {age}")
+
+get_average_larguest()
