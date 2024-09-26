917339d6a7e035c0da59a66754b14857bb0c8da4
santiago galindo <santiago1593572684@gmail.com>
2024-09-26 01:47:11 +0000

    lab-3

--- semanatres/lab_3.py
+++ semanatres/lab_3.py
@@ -20,7 +20,7 @@ def generate_people(num_people: int):
         person = {
             "dni": i + 1,
             "name": fake.first_name(),
-            "lasname": fake.last_name(),
+            "lastname": fake.last_name(),
             "role": random.randint(1,3)
 
         }
@@ -36,16 +36,27 @@ def select_role():
             person["role"] = "profesor"
         else:
             person["role"]= "estudiante"
-        
 
+def match_role():
+    for person in people_list:
+        match person["role"]:
+            case 1:
+                person["role"]= "estudiante"
+            case 2:
+                person["role"]= "profesor"
+            case 3:
+                person["role"]= "estudiante"
 
 
 def print_people():
     for person in people_list:
         print(person)
 
+
+
 number=int(input("por favor ingrese la cantidad de usuarios:\n"))
 generate_people(number)
 select_role()
+match_role()
 print_people()
 
